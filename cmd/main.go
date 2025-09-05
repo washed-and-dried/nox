ef1198aa231a8f9cd0275b57fe850df47cc15933
bharatm29 <119489267+bharatm29@users.noreply.github.com>
2025-09-05 09:34:10 +0530

    can pass file through args now

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"nox/internals/eval"
 	"nox/internals/parser"
+	"os"
 )
 
 func main() {
-	file := "examples/triangle.nox"
+	args := os.Args
+	if (len(args) != 2) {
+		fmt.Printf("Invalid number of arguments: %d\n", len(args))
+		return
+	}
+
+	file := args[1]
 	p := parser.NewParser(file)
 	program := p.Parse_program()
 	eval.Eval_program(program)
